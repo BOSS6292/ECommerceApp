b68e22a259f0929b71e18a6f79fbb5e862218382
TEJAS <tbomble6292@gmail.com>
2023-04-24 13:06:47 +0530

    Added Dependencies

---
+++ app/src/main/java/com/boss/ecommerceapp/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.boss.ecommerceapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.boss.ecommerceapp.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/com/boss/ecommerceapp/activities/ShoppingActivity.kt
+++ app/src/main/java/com/boss/ecommerceapp/activities/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.boss.ecommerceapp
+package com.boss.ecommerceapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.boss.ecommerceapp.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
